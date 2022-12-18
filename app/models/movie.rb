61b95dfd68ca7319d1773ee0359a4e2622a5c01c
Kevin Diesenberg <kdiesenb@gmail.com>
2022-12-18 17:31:11 -0500

    class vs instance method is still odd to me.  gonna go review the basics

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -18,6 +18,38 @@ class Movie < ActiveRecord::Base
     Movie.last
   end
 
-  
+  def self.movie_count
+    Movie.count
+  end
+
+  def self.find_movie_with_id(id)
+    Movie.find_by_id(id)
+  end
+
+  def self.find_movie_with_attributes(attributes)
+    Movie.find_by(attributes)
+  end
+
+  def self.find_movies_after_2002
+    Movie.where("release_date > 2002")
+  end
+
+  def update_with_attributes(attributes)
+    self.update(attributes)
+  end
+
+  def self.update_all_titles(title)
+    Movie.update(title: title)
+  end
+
+  def self.delete_by_id(id)
+    movie = Movie.find(id)
+    movie.destroy
+  end
+
+  def self.delete_all_movies
+    Movie.destroy_all
+  end
+
 end
 
